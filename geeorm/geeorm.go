bee21f0bdef3e5fef89efa1c7bd58a6e9dd17a51
justniu <1573996821@qq.com>
2021-07-01 18:18:50 +0800

    object table struct mapping

--- geeorm/geeorm.go
+++ geeorm/geeorm.go
@@ -2,12 +2,14 @@ package geeorm
 
 import (
 	"database/sql"
-	"geeorm/session"
+	"geeorm/dialect"
 	"geeorm/log"
+	"geeorm/session"
 )
 
 type Engine struct {
 	db *sql.DB 
+	dialect dialect.Dialect 
 }
 
 func NewEngine(driver, source string) (e *Engine, err error) {
@@ -23,7 +25,14 @@ func NewEngine(driver, source string) (e *Engine, err error) {
 		return 
 	}
 
-	e = &Engine{db: db}
+	// make sure the specific dialect exists
+	dial, ok := dialect.GetDialect(driver)
+	if !ok {
+		log.Errorf("dialect %s Not Found", driver)
+		return 
+	}
+
+	e = &Engine{db: db, dialect:dial}
 	log.Info("Connection: database success")
 	return 
 }
@@ -36,5 +45,5 @@ func (e *Engine) Close() {
 }
 
 func (e *Engine) NewSession() *session.Session{
-	return session.New(e.db)
+	return session.New(e.db, e.dialect)
 }
